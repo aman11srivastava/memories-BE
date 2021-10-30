55177b01768b86043fd8a4c2ba5ab629f1ca11d0
Aman Srivastava <aman.srivastava@thoughtwizards.com>
2021-10-30 16:06:31 +0530

    Cors bug fixed

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes)
-
 app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors());
 dotenv.config();
 
+app.use('/posts', postRoutes)
 const CONNECTION_URL = process.env.MONGODB_CONNECTION_STRING;
 const PORT = process.env.PORT || 5000;
 
